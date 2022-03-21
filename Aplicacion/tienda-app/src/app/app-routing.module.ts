ad54cc1c43f4cc528f9918e0580c4868bd304f4c
Sneyder <sneider.caicedo@gmail.com>
2022-03-21 16:19:52 -0500

    Aplicacion base

---
+++ Aplicacion/tienda-app/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'message/:id',
+    loadChildren: () => import('./detail-client/view-message.module').then( m => m.ViewMessagePageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
