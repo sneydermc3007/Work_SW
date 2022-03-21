3b642b21ab3f2de59dbeabc15921e926f7eaa373
Sneyder <sneider.caicedo@gmail.com>
2022-03-21 14:46:43 -0500

    Correcion

--- Reto #1/division_factorial.py
+++ Reto #1/division_factorial.py
@@ -27,7 +27,6 @@ def main():
     if (fact and primo > 0):
         is_not_primo = not es_primo(primo)
 
-<<<<<<< HEAD
         while is_not_primo:
             primo = int(input("Vuelva a intentar con nuevo numero para dividir el factorial: "))
             is_not_primo = not es_primo(primo)
@@ -40,36 +39,6 @@ def main():
                 print("NO")
         else:
             print(f"N que es: {fact} & P que es: {primo}, son mayores al maximo permitido.")
-=======
-    if (fact and primo > 0) and (fact and primo < 2**31):
-        
-        def es_primo(numero):
-            for i in range(numero):
-                if numero % i == 0:
-                    print(f"\n El numero ingresado no es primo tiene a {i} de divisor")
-                    return False
-                else:   
-                    print(f"\n {numero} es primo")
-                    return True
-
-        respuesta = es_primo(primo)
-        if(respuesta == True):
-
-            def factorial(numero):
-                if numero < 0:
-                    return("no exite ya que es numero negativo")
-                elif numero == 0:
-                    return 1
-                else:
-                    resultado = 1
-                    while (numero > 1):
-                        resultado *= numero
-                        numero -= 1
-                    return resultado
-
-    elif (fact and primo > 2**31):
-        print(f"N que es: {fact} & P que es: {primo}, son mayores al maximo permitido.")
->>>>>>> master
     else:
         print("Fin.")
 
