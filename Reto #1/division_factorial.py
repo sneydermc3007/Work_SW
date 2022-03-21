c702a6e85a1f4260470132734a166b2497ec91dc
Sneyder <sneider.caicedo@gmail.com>
2022-03-20 20:20:30 -0500

    Numero primo

--- Reto #1/division_factorial.py
+++ Reto #1/division_factorial.py
@@ -1,8 +1,10 @@
 def main():
     
     fact = int(input('Ingrese el numero al que desea sacar el factorial: '))
+    primo = int(input('Ingrese el numero por el que desea dividir el factorial: '))
 
-    if fact < 2**31:
+
+    if (fact and primo > 0) and (fact and primo < 2**31):
         def factorial(numero):
 
             if numero < 0:
@@ -17,9 +19,20 @@ def main():
                 return resultado
 
         print(f"El factorial de {fact} es: ", factorial(fact))
-    
+
+        def es_primo(numero):
+            for n in range(2, numero):
+                if numero % n == 0:
+                    print(f"El numero ingresado no es primo tiene a {n} divisor")
+                    return False
+            print(f"{numero} es primo")
+            return True
+
+        print(f"Este numero {primo} es: ", es_primo(primo))
+
+        
     else:
-        print(f"El numero {fact} es mayor que lo permitido. ")
+        print(f"N que es: {fact} & P que es: {primo}, no cumplen con lo establecido. ")
 
 if __name__ == '__main__':
     main()
