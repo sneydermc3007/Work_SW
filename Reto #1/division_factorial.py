2486f5b6b3cf377b6bfef66346220e07aa1cc8b7
Sneyder <sneider.caicedo@gmail.com>
2022-03-21 14:20:38 -0500

    Reto numero 1 finalizado

--- Reto #1/division_factorial.py
+++ Reto #1/division_factorial.py
@@ -1,18 +1,18 @@
 def main():
 
     def es_primo(numero):
-        for i in range(numero):
-            if numero % i == 0:
-                print(f"\n El numero ingresado no es primo tiene a {i} de divisor")
-                return False
-            else:   
-                print(f"\n {numero} es primo")
-                return True
+        if (numero == 1) or (numero == 2):
+            return True
+        else:
+            for i in range(2, numero):
+                if (numero % i) == 0:
+                    print(f"\n El numero ingresado no es primo tiene a {i} de divisor")
+                    return False
+                else:
+                    return True
 
     def factorial(numero):
-        if numero < 0:
-            return("no exite ya que es numero negativo")
-        elif numero == 0:
+        if numero == 0:
             return 1
         else:
             resultado = 1
@@ -21,17 +21,24 @@ def main():
                 numero -= 1
             return resultado
     
-    fact = int(input('Ingrese el numero al que desea sacar el factorial: '))
-    primo = int(input('Ingrese el numero por el que desea dividir el factorial: '))
+    primo = int(input("Ingrese el numero por el que desea dividir el factorial: "))
+    fact = int(input("Ingrese el numero del que desea conocer el factorial: "))
 
-    if (fact and primo > 0) and (fact and primo < 2**31):
-    
-        respuesta = es_primo(primo)
-        if(respuesta == False):
-            pass
+    if (fact and primo > 0):
+        is_not_primo = not es_primo(primo)
 
-    elif (fact and primo > 2**31):
-        print(f"N que es: {fact} & P que es: {primo}, son mayores al maximo permitido.")
+        while is_not_primo:
+            primo = int(input("Vuelva a intentar con nuevo numero para dividir el factorial: "))
+            is_not_primo = not es_primo(primo)
+
+        if fact < 2**31 and primo < 2**31:
+            resultado = factorial(fact)
+            if (resultado %  primo)== 0:
+                print("YES")
+            else:
+                print("NO")
+        else:
+            print(f"N que es: {fact} & P que es: {primo}, son mayores al maximo permitido.")
     else:
         print("Fin.")
 
