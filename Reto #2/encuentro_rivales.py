41d197aac22e7c04d4f3b4b2df053c03560f82e0
Snake <sneider.caicedo@gmail.com>
2022-03-22 12:32:49 -0500

    Reto 2 Finalizado

--- Reto #2/encuentro_rivales.py
+++ Reto #2/encuentro_rivales.py
@@ -5,18 +5,38 @@ def main():
     if participantes > 0 and participantes <= 20:
 
         participantes = 2**participantes
-        print(f"El numero de participantes en el torneo es de: {participantes}")
+        print(f"\nEl numero de participantes que hay en el torneo es de: {participantes}")
     
         for i in range(participantes):
-            prueba = int(input(f'Ingrese el numero del jugador en posicion {i}: ')) # Esta linea es de prueba
-            torneo.append(prueba)
+            torneo.append(i+1)
 
-        print(torneo)
+        print(f"\nEste es el orden de participantes: {torneo}")
+        player1 = int(input("\tIngrese el primer jugador del enfrentamiento: "))
+        player2 = int(input("\tIngrese el otro jugador con el que se va a enfrentar: "))
+
+        if((1 <= player1 <= 2**participantes) and ((1 <= player2 <= 2**participantes) and player1 != player2)):
+            round = 0
+            p1 = player1 - 1
+            p2 = player2 - 1
+
+            while p1 != p2:
+                p1 >>= 1
+                p2 >>= 1
+                round += 1
+
+            print(f"\nLos jugadores {player1} y {player2} se enfrentaran en la ronda: {round}")
+
+        elif((1 <= player1 <= 2**participantes) and ((1 <= player2 <= 2**participantes) and player1 == player2)):
+            print("\nLos jugadores para enfrentar son los mismos")
+            print("Fin.")
+        else:
+            print("\nUno o ambos jugadores no estan inscritos en el torneo")
+            print("Fin.")
 
     elif participantes > 20:
-        print(f"El numero ingresado {participantes} es mayor que lo permitido.")
+        print(f"\nEl numero ingresado {participantes} es mayor que lo permitido.")
     else:
-        print(f"El numero ingresado {participantes} es menor que lo permitido.")
+        print(f"\nEl numero ingresado {participantes} es menor que lo permitido.")
 
 if __name__ == '__main__':
     main()
