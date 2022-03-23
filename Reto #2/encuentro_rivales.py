9c4850bd8cafd0e833eded6adc0dad9e2211af2b
Sneyder <sneider.caicedo@gmail.com>
2022-03-23 09:12:23 -0500

    Update reto 2
    Un mejor lexico en los print

--- Reto #2/encuentro_rivales.py
+++ Reto #2/encuentro_rivales.py
@@ -1,18 +1,20 @@
 def main():
-    torneo = []
-    participantes = int(input('Ingrese un numero: '))
+    
+    n = int(input('Ingrese un numero de rondas que desea en el torneo: '))
 
-    if participantes > 0 and participantes <= 20:
-
-        participantes = 2**participantes
-        print(f"\nEl numero de participantes que hay en el torneo es de: {participantes}")
+    if n > 0 and n <= 20:
+        
+        torneo = []
+        participantes = 2**n
+        print(f"\nEl número de jugadores que hay en el torneo es de: {participantes}")
     
         for i in range(participantes):
             torneo.append(i+1)
 
-        print(f"\nEste es el orden de participantes: {torneo}")
-        player1 = int(input("\tIngrese el primer jugador del enfrentamiento: "))
-        player2 = int(input("\tIngrese el otro jugador con el que se va a enfrentar: "))
+        print(f"\tEste es el orden de participantes: {torneo}")
+
+        player1 = int(input("\n\tIngrese el número de serie del Primer jugador: "))
+        player2 = int(input("\tIngrese el número de serie del Segundo jugador: "))
 
         if((1 <= player1 <= 2**participantes) and ((1 <= player2 <= 2**participantes) and player1 != player2)):
             round = 0
@@ -30,13 +32,13 @@ def main():
             print("\nLos jugadores para enfrentar son los mismos")
             print("Fin.")
         else:
-            print("\nUno o ambos jugadores no estan inscritos en el torneo")
+            print("\nAlguno de los números de serie ingresado, no se encuentra entre el rango de jugadores")
             print("Fin.")
 
-    elif participantes > 20:
-        print(f"\nEl numero ingresado {participantes} es mayor que lo permitido.")
+    elif n > 20:
+        print(f"\nEl numero ingresado {n} es mayor que lo permitido que es 20.")
     else:
-        print(f"\nEl numero ingresado {participantes} es menor que lo permitido.")
+        print(f"\nEl numero ingresado {n} es menor que lo permitido que es 1.")
 
 if __name__ == '__main__':
     main()
