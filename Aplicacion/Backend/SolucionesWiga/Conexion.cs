fb42b0839446245006136318436f2f7aa6912337
Sneyder <sneider.caicedo@gmail.com>
2022-03-29 09:26:01 -0500

    Clase Conexion

---
+++ Aplicacion/Backend/SolucionesWiga/Conexion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SolucionesWiga
+{
+    public class Conexion
+    {
+        public static MySqlConnection ObtenerConexion()
+        {
+
+            MySqlConnection conectar = new MySqlConnection("server=127.0.0.1; database=solucioneswiga; Uid=root; pwd=;");
+
+            conectar.Open();
+
+            return conectar;
+        }
+    }
+}
